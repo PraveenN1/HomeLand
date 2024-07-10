6fd17d7fb641401b42913f3e30cad3a3130588d7
Praveen Nayakar <105967145+PraveenN1@users.noreply.github.com>
2024-07-10 23:00:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 
-app.use(cors());
+app.use(cors(
+  {
+  origin:["https://home-land-frpu.vercel.app"],
+  methods:["POST","GET"],
+  credentials:true
+  }
+));
 
 app.get('/', (req, res) => {
   res.send('Welcome to the home page!');
